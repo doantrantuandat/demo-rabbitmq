09b1b97308e1550ba304803e10dd55790ab744c6
doantrantuandat <doantrantuandat.98@gmail.com>
2023-05-14 09:09:17 +0700

    complete demo rabitmq

--- src/main/java/com/example/sender/MessageSender.java
+++ src/main/java/com/example/sender/MessageSender.java
@@ -1,19 +1,31 @@
 package com.example.sender;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@Component
-public class MessageSender {
-    private RabbitTemplate rabbitTemplate;
+@SpringBootApplication
+public class MessageSender implements CommandLineRunner {
 
     @Autowired
-    public MessageSender(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
+    private RabbitTemplate rabbitTemplate;
+
+    @Bean
+    public Queue queue() {
+        return new Queue("myQueue");
     }
 
-    public void sendMessage(String message) {
+    public static void main(String[] args) {
+        SpringApplication.run(MessageSender.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String message = "Hello, RabbitMQ!";
         rabbitTemplate.convertAndSend("myQueue", message);
         System.out.println("Message sent: " + message);
     }
